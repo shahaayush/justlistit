1810919748467d92e3d7d5ae0accc1738a9521ad
Aayush Shah <shahaayush@outlook.com>
2019-11-24 16:25:47 +0530

    added basic user authentication and django models

--- justlist/frontend/urls.py
+++ justlist/frontend/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 urlpatterns = [
-  path('', views.index)
+  path('home/', views.index, name='home'),
+  path('login/', auth_views.LoginView.as_view(template_name = "frontend/login.html"), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(next_page = "/login"), name='logout'),
+  path('signup/', views.signup, name='signup'),
 ]
--- justlist/todoapp/models.py
+++ justlist/todoapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100) 
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+    def __str__(self):
+        return self.name
+
+class TodoList(models.Model):
+    # title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+    is_marked = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, default="general", on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ["-created"]
+    def __str__(self):
+        return self.title
